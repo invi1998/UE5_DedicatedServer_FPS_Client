d9d87f990826e9b5799f5e1d61056861eb0c36fe
invi1998 <2267661826@qq.com>
2024-12-05 18:04:28 +0800

    Game Session Struct

--- Source/DedicatedServers/Private/UI/HTTP/HTTPRequestTypes.cpp
+++ Source/DedicatedServers/Private/UI/HTTP/HTTPRequestTypes.cpp
@@ -7,19 +7,48 @@
 void FDSMeataData::Dump() const
 {
 	UE_LOG(LogDedicatedServers, Log, TEXT("metadata:"));
-	UE_LOG(LogDedicatedServers, Log, TEXT("HTTP Status Code: %d"), httpStatusCode);
-	UE_LOG(LogDedicatedServers, Log, TEXT("Request ID: %s"), *requestId);
-	UE_LOG(LogDedicatedServers, Log, TEXT("Attempts: %d"), attempts);
-	UE_LOG(LogDedicatedServers, Log, TEXT("Total Retry Delay: %f"), totalRetryDelay);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	HTTP Status Code: %d"), httpStatusCode);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	Request ID: %s"), *requestId);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	Attempts: %d"), attempts);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	Total Retry Delay: %f"), totalRetryDelay);
 }
 
 void FDSListFleetsResponse::Dump() const
 {
 	UE_LOG(LogDedicatedServers, Log, TEXT("ListFleetsResponse:"));
-	UE_LOG(LogDedicatedServers, Log, TEXT("FleetIds:"));
+	UE_LOG(LogDedicatedServers, Log, TEXT("	FleetIds:"));
 	for (const FString& FleetId : FleetIds)
 	{
-		UE_LOG(LogDedicatedServers, Log, TEXT("  %s"), *FleetId);
+		UE_LOG(LogDedicatedServers, Log, TEXT("	  %s"), *FleetId);
 	}
-	UE_LOG(LogDedicatedServers, Log, TEXT("NextToken: %s"), *NextToken);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	NextToken: %s"), *NextToken);
+}
+
+void FDSGameSession::Dump() const
+{
+	UE_LOG(LogDedicatedServers, Log, TEXT("GameSession:"));
+	UE_LOG(LogDedicatedServers, Log, TEXT("	CreationTime: %lld"), CreationTime);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	CreatorId: %s"), *CreatorId);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	CurrentPlayerSessionCount: %d"), CurrentPlayerSessionCount);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	DnsName: %s"), *DnsName);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	FleetArn: %s"), *FleetArn);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	FleetId: %s"), *FleetId);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	GameProperties:"));
+	for (const TPair<FString, FString>& GameProperty : GameProperties)
+	{
+		UE_LOG(LogDedicatedServers, Log, TEXT("	  %s: %s"), *GameProperty.Key, *GameProperty.Value);
+	}
+	UE_LOG(LogDedicatedServers, Log, TEXT("	GameSessionData: %s"), *GameSessionData);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	GameSessionId: %s"), *GameSessionId);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	IpAddress: %s"), *IpAddress);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	Location: %s"), *Location);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	MatchmakerData: %s"), *MatchmakerData);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	MaximumPlayerSessionCount: %d"), MaximumPlayerSessionCount);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	Name: %s"), *Name);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	PlayerSessionCreationPolicy: %s"), *PlayerSessionCreationPolicy);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	Port: %d"), Port);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	Status: %s"), *Status);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	StatusReason: %s"), *StatusReason);
+	UE_LOG(LogDedicatedServers, Log, TEXT("	TerminationTime: %lld"), TerminationTime);
+	
 }
--- Source/DedicatedServers/Private/UI/HTTP/HTTPRequestTypes.h
+++ Source/DedicatedServers/Private/UI/HTTP/HTTPRequestTypes.h
@@ -37,3 +37,100 @@ struct FDSListFleetsResponse
 
 	void Dump() const;
 };
+
+/*
+{
+	"GameSession": { 
+		"CreationTime": number,
+		"CreatorId": "string",
+		"CurrentPlayerSessionCount": number,
+		"DnsName": "string",
+		"FleetArn": "string",
+		"FleetId": "string",
+		"GameProperties": [ 
+		   { 
+			  "Key": "string",
+			  "Value": "string"
+		   }
+		],
+		"GameSessionData": "string",
+		"GameSessionId": "string",
+		"IpAddress": "string",
+		"Location": "string",
+		"MatchmakerData": "string",
+		"MaximumPlayerSessionCount": number,
+		"Name": "string",
+		"PlayerSessionCreationPolicy": "string",
+		"Port": number,
+		"Status": "string",
+		"StatusReason": "string",
+		"TerminationTime": number
+	 }
+}
+*/
+
+USTRUCT()
+struct FDSGameSession
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	int64 CreationTime{};						// 创建时间
+
+	UPROPERTY()
+	FString CreatorId{};						// 创建者ID
+
+	UPROPERTY()
+	int32 CurrentPlayerSessionCount{};			// 当前玩家会话计数
+
+	UPROPERTY()
+	FString DnsName{};							// DNS名称
+
+	UPROPERTY()
+	FString FleetArn{};							// 舰队ARN
+
+	UPROPERTY()
+	FString FleetId{};							// 舰队ID
+
+	UPROPERTY()
+	TMap<FString, FString> GameProperties{};	// 游戏属性
+
+	UPROPERTY()
+	FString GameSessionData{};					// 游戏会话数据
+
+	UPROPERTY()
+	FString GameSessionId{};					// 游戏会话ID
+
+	UPROPERTY()
+	FString IpAddress{};						// IP地址
+
+	UPROPERTY()
+	FString Location{};							// 位置
+
+	UPROPERTY()
+	FString MatchmakerData{};					// 匹配数据
+
+	UPROPERTY()
+	int32 MaximumPlayerSessionCount{};			// 最大玩家会话计数
+
+	UPROPERTY()
+	FString Name{};								// 名称
+
+	UPROPERTY()
+	FString PlayerSessionCreationPolicy{};		// 玩家会话创建策略
+
+	UPROPERTY()
+	int32 Port{};								// 端口
+
+	UPROPERTY()
+	FString Status{};							// 状态
+
+	UPROPERTY()
+	FString StatusReason{};						// 状态原因
+
+	UPROPERTY()
+	int64 TerminationTime{};					// 终止时间
+
+	void Dump() const;
+};
+

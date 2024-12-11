2c70e897a66da494bdf04d98c57e2d1fc2c3f12f
invi1998 <2267661826@qq.com>
2024-12-11 15:20:55 +0800

    Dropdown

---
+++ Source/DedicatedServers/Private/UI/portal/Dropdown/Dropdown.cpp
@@ -0,0 +1,78 @@
+﻿// // Copyright invi1998
+
+
+#include "UI/portal/Dropdown/Dropdown.h"
+
+#include "Components/Button.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "Components/WidgetSwitcher.h"
+
+void UDropdown::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	Button_Expander->OnClicked.AddDynamic(this, &UDropdown::ToggleDropdown);
+	Button_Expander->OnHovered.AddDynamic(this, &UDropdown::Hover);
+	Button_Expander->OnUnhovered.AddDynamic(this, &UDropdown::UnHover);
+}
+
+void UDropdown::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	SetStyleTransparent();
+	Collapse();
+	UnHover();
+}
+
+void UDropdown::SetStyleTransparent()
+{
+	FButtonStyle ButtonStyle;
+	FSlateBrush NormalBrush = TriangleDown_Brush;
+
+	NormalBrush.TintColor = FSlateColor(FLinearColor(1.0f, 1.0f, 1.0f, 0.0f));
+	ButtonStyle.Disabled = NormalBrush;
+	ButtonStyle.Normal = NormalBrush;
+	ButtonStyle.Hovered = NormalBrush;
+	ButtonStyle.Pressed = NormalBrush;
+	
+	Button_Expander->SetStyle(ButtonStyle);
+}
+
+
+void UDropdown::ToggleDropdown()
+{
+	if (bIsExpanded)
+	{
+		Collapse();
+	}
+	else
+	{
+		Expand();
+	}
+}
+
+void UDropdown::Expand()
+{
+	WidgetSwitcher->SetActiveWidget(ExpandedWidget);
+	bIsExpanded = true;
+	Image_Triangle->SetBrush(TriangleUp_Brush);
+}
+
+void UDropdown::Collapse()
+{
+	WidgetSwitcher->SetActiveWidget(CollapsedWidget);
+	bIsExpanded = false;
+	Image_Triangle->SetBrush(TriangleDown_Brush);
+}
+
+void UDropdown::Hover()
+{
+	TextBlock_ButtonText->SetColorAndOpacity(HoveredTextColor);
+}
+
+void UDropdown::UnHover()
+{
+	TextBlock_ButtonText->SetColorAndOpacity(NormalTextColor);
+}
---
+++ Source/DedicatedServers/Public/UI/portal/Dropdown/Dropdown.h
@@ -0,0 +1,76 @@
+﻿// // Copyright invi1998
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Dropdown.generated.h"
+
+class UImage;
+class UTextBlock;
+class UButton;
+class UWidgetSwitcher;
+/**
+ * 
+ */
+UCLASS()
+class DEDICATEDSERVERS_API UDropdown : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UWidgetSwitcher> WidgetSwitcher;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UUserWidget> ExpandedWidget;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UUserWidget> CollapsedWidget;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> Button_Expander;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> TextBlock_ButtonText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dropdown")
+	FSlateColor HoveredTextColor = FSlateColor(FLinearColor(0.0f, 0.0f, 0.0f, 1.0f));
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dropdown")
+	FSlateColor NormalTextColor = FSlateColor(FLinearColor(1.0f, 1.0f, 1.0f, 1.0f));
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dropdown")
+	FSlateBrush TriangleUp_Brush;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dropdown")
+	FSlateBrush TriangleDown_Brush;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dropdown")
+	TObjectPtr<UImage> Image_Triangle;
+
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativePreConstruct() override;
+
+	void SetStyleTransparent();
+
+private:
+	UFUNCTION()
+	void ToggleDropdown();
+
+	bool bIsExpanded = false;
+
+	UFUNCTION()
+	void Expand();
+
+	UFUNCTION()
+	void Collapse();
+
+	UFUNCTION()
+	void Hover();
+
+	UFUNCTION()
+	void UnHover();
+
+};

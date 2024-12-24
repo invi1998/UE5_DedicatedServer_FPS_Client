266adaec9e44267520af99e9bf51e29d3f6737ce
invi1998 <2267661826@qq.com>
2024-12-24 18:34:46 +0800

    TimerWidget

---
+++ Source/DedicatedServers/Private/UI/Timers/TimerWidget.cpp
@@ -0,0 +1,96 @@
+﻿// // Copyright invi1998
+
+
+#include "UI/Timers/TimerWidget.h"
+
+#include "Components/TextBlock.h"
+#include "Kismet/KismetStringLibrary.h"
+#include "Player/DSPlayerController.h"
+
+void UTimerWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	OwningPlayerController = Cast<ADSPlayerController>(GetOwningPlayer());
+	if (IsValid(OwningPlayerController))
+	{
+		OwningPlayerController->OnTimerUpdated.AddDynamic(this, &UTimerWidget::OnTimerUpdated);
+		OwningPlayerController->OnTimerFinished.AddDynamic(this, &UTimerWidget::OnTimerFinished);
+	}
+
+	if (bHideOnZero)
+	{
+		TextBlock_Timer->SetVisibility(ESlateVisibility::Hidden);
+	}
+}
+
+void UTimerWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	if (bActive)
+	{
+		UpdateCountdownTime(InternalCountdownTimeLeft - InDeltaTime);
+	}
+}
+
+void UTimerWidget::OnTimerUpdated(float CountdownTimeLeft, ECountdownTimerType InTimerType)
+{
+	if (InTimerType != TimerType) return;
+	if (!bActive)
+	{
+		TimerStart(CountdownTimeLeft);
+	}
+	UpdateCountdownTime(CountdownTimeLeft);
+	K2_OnTimerUpdated(CountdownTimeLeft, TimerType);
+}
+
+void UTimerWidget::OnTimerFinished(float CountdownTimeLeft, ECountdownTimerType InTimerType)
+{
+	if (InTimerType != TimerType) return;
+	TimerStop();
+	K2_OnTimerFinished(0.f, TimerType);
+}
+
+FString UTimerWidget::FormatTimeAsString(float TimeInSeconds) const
+{
+	TimeInSeconds = bCanBeNegative ? TimeInSeconds : FMath::Abs(TimeInSeconds);
+	FString DisplayTimeString{};
+	if (bShowMilliseconds)
+	{
+		DisplayTimeString = UKismetStringLibrary::TimeSecondsToString(TimeInSeconds);
+	}
+	else
+	{
+		const TCHAR* NegativeModifier = TimeInSeconds < 0.f? TEXT("-") : TEXT("");
+		TimeInSeconds = FMath::Abs(TimeInSeconds);
+	
+		const int32 NumMinutes = FMath::FloorToInt(TimeInSeconds/60.f);
+		const int32 NumSeconds = FMath::FloorToInt(TimeInSeconds-(NumMinutes*60.f));
+		DisplayTimeString = FString::Printf(TEXT("%s%02d:%02d"), NegativeModifier, NumMinutes, NumSeconds);
+	}
+	return DisplayTimeString;
+}
+
+void UTimerWidget::TimerStart(float InitialTime)
+{
+	bActive = true;
+	TextBlock_Timer->SetVisibility(ESlateVisibility::Visible);
+}
+
+void UTimerWidget::TimerStop()
+{
+	bActive = false;
+	UpdateCountdownTime(0.f);
+	if (bHideOnZero)
+	{
+		TextBlock_Timer->SetVisibility(ESlateVisibility::Hidden);
+	}
+}
+
+void UTimerWidget::UpdateCountdownTime(float NewTime)
+{
+	InternalCountdownTimeLeft = NewTime;
+	TextBlock_Timer->SetText(FText::FromString(FormatTimeAsString(InternalCountdownTimeLeft)));
+}
+
---
+++ Source/DedicatedServers/Public/UI/Timers/TimerWidget.h
@@ -0,0 +1,66 @@
+﻿// // Copyright invi1998
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Types/DSTypes.h"
+#include "TimerWidget.generated.h"
+
+class UTextBlock;
+class ADSPlayerController;
+/**
+ * 
+ */
+UCLASS()
+class DEDICATEDSERVERS_API UTimerWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct() override;
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+protected:
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	ECountdownTimerType TimerType = ECountdownTimerType::NONE;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<ADSPlayerController> OwningPlayerController;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bActive = false;	// 是否激活
+
+	UPROPERTY(BlueprintEditDefaultsOnly)
+	bool bCanBeNegative = false;	// 是否可以为负数
+
+	UPROPERTY(BlueprintEditDefaultsOnly)
+	bool bShowMilliseconds = false;	// 是否显示毫秒
+
+	UPROPERTY(BlueprintEditDefaultsOnly)
+	bool bHideOnZero = false;	// 是否在0时隐藏
+
+	UFUNCTION()
+	virtual void OnTimerUpdated(float CountdownTimeLeft, ECountdownTimerType InTimerType);
+
+	UFUNCTION()
+	virtual void OnTimerFinished(float CountdownTimeLeft, ECountdownTimerType InTimerType);
+
+	FString FormatTimeAsString(float TimeInSeconds) const;
+
+	UFUNCTION(BlueprintImplementableEvent, meta=(DisplayName="On Timer Updated"))
+	void K2_OnTimerUpdated(float CountdownTimeLeft, ECountdownTimerType InTimerType);
+
+	UFUNCTION(BlueprintImplementableEvent, meta=(DisplayName="On Timer Finished"))
+	void K2_OnTimerFinished(float CountdownTimeLeft, ECountdownTimerType InTimerType);
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> TextBlock_Timer;
+
+	float InternalCountdownTimeLeft = 0.0f;
+
+	void TimerStart(float InitialTime);
+	void TimerStop();
+	void UpdateCountdownTime(float NewTime);
+};

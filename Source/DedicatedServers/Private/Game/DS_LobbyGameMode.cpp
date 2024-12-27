64799f63a5586904842cf13b01b6320de4fb1f77
invi1998 <2267661826@qq.com>
2024-12-27 13:49:14 +0800

    Lobby Timer

--- Source/DedicatedServers/Private/Game/DS_LobbyGameMode.cpp
+++ Source/DedicatedServers/Private/Game/DS_LobbyGameMode.cpp
@@ -5,17 +5,87 @@
 
 #include "DedicatedServers/DedicatedServers.h"
 #include "Game/DS_GameInstanceSubsystem.h"
+#include "Kismet/GameplayStatics.h"
 
 ADS_LobbyGameMode::ADS_LobbyGameMode()
 {
+	bUseSeamlessTravel = true;
 	LobbyStatus = ELobbyStatus::WaitingForPlayers;
+	LobbyCountdownTimer.Type = ECountdownTimerType::LobbyCountdown;
+	LobbyCountdownTimer.Duration = 10.0f;
+	LobbyCountdownTimer.UpdateInterval = 1.0f;
+	
+}
+
+void ADS_LobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+	
+	CheckAndStartLobbyCountdown();
+}
+
+void ADS_LobbyGameMode::Logout(AController* Exiting)
+{
+	Super::Logout(Exiting);
+	
+	CheckAndStopLobbyCountdown();
 }
 
 void ADS_LobbyGameMode::BeginPlay()
 {
 	Super::BeginPlay();
-
+	
 	InitGameLift();
+}
+
+void ADS_LobbyGameMode::InitSeamlessTravelPlayer(AController* NewController)
+{
+	Super::InitSeamlessTravelPlayer(NewController);
+	
+	CheckAndStartLobbyCountdown();
+}
+
+void ADS_LobbyGameMode::OnCountdownTimerFinished(ECountdownTimerType InTimerType)
+{
+	Super::OnCountdownTimerFinished(InTimerType);
+
+	if (InTimerType == ECountdownTimerType::LobbyCountdown)
+	{
+		// 无缝旅行
+		LobbyStatus = ELobbyStatus::SeamlessTravel;
+		const FString DestinationMapPath = DestinationMap.ToSoftObjectPath().GetAssetName();
+		UE_LOG(LogDedicatedServers, Log, TEXT("Seamless Travel to %s"), *DestinationMapPath)
+		if (GIsEditor)
+		{
+			// 编辑器模式下，直接切换地图
+			UGameplayStatics::OpenLevel(GetWorld(), FName(*DestinationMapPath));
+		}
+		else
+		{
+			// 服务器模式下，无缝旅行
+			GetWorld()->ServerTravel(DestinationMapPath, true, false);
+		}
+	}
+}
+
+void ADS_LobbyGameMode::CheckAndStartLobbyCountdown()
+{
+	if (GetNumPlayers() >= MinPlayers && LobbyStatus == ELobbyStatus::WaitingForPlayers)
+	{
+		// 开始无缝旅行
+		LobbyStatus = ELobbyStatus::CountdownToSeamlessTravel;
+		StartCountdownTimer(LobbyCountdownTimer);
+	}
+}
+
+void ADS_LobbyGameMode::CheckAndStopLobbyCountdown()
+{
+	if (GetNumPlayers()-1 < MinPlayers && LobbyStatus == ELobbyStatus::CountdownToSeamlessTravel)
+	{
+		// 取消无缝旅行
+		LobbyStatus = ELobbyStatus::WaitingForPlayers;
+		StopCountdownTimer(LobbyCountdownTimer);
+	}
 }
 
 void ADS_LobbyGameMode::InitGameLift()
@@ -62,5 +132,5 @@ void ADS_LobbyGameMode::SetServerParameters(FServerParameters& OutServerParamete
 
 	// 进程ID
 	OutServerParameters.m_processId = FString::Printf(TEXT("%d"), FPlatformProcess::GetCurrentProcessId());
-	UE_LOG(LogDS_GameMode, Log, TEXT("PROCESS_ID [PID]: %s"), *OutServerParameters.m_processId)
+	UE_LOG(LogDedicatedServers, Log, TEXT("PROCESS_ID [PID]: %s"), *OutServerParameters.m_processId)
 }
--- Source/DedicatedServers/Public/Game/DS_LobbyGameMode.h
+++ Source/DedicatedServers/Public/Game/DS_LobbyGameMode.h
@@ -18,19 +18,36 @@ class DEDICATEDSERVERS_API ADS_LobbyGameMode : public ADS_GameModeBase
 
 public:
 	ADS_LobbyGameMode();
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+	virtual void Logout(AController* Exiting) override;	// 玩家退出
 
 protected:
 	virtual void BeginPlay() override;
+	virtual void InitSeamlessTravelPlayer(AController* NewController) override;		// 初始化无缝旅行玩家
+	virtual void OnCountdownTimerFinished(ECountdownTimerType InTimerType) override;
+
+	void CheckAndStartLobbyCountdown();
+	void CheckAndStopLobbyCountdown();
 
 	UPROPERTY()
 	ELobbyStatus LobbyStatus;
+	
+	UPROPERTY(EditDefaultsOnly)
+	int32 MinPlayers = 1;
+
+	// 无缝旅行目标地图
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<UWorld> DestinationMap;
 
 private:
 	UPROPERTY()
 	TObjectPtr<UDS_GameInstanceSubsystem> DSGameInstanceSubsystem;
 
-	FProcessParameters ProcessParameters;
-
+	UPROPERTY(EditDefaultsOnly)
+	FCountdownTimerHandle LobbyCountdownTimer;
+	
 	void InitGameLift();
 	void SetServerParameters(FServerParameters& OutServerParameters);
+	
 };
+

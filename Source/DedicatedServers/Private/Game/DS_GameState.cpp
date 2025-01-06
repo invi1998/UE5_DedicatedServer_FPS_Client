e96b589c3eb59f9b9ef40b4b29096b39824cc1a3
invi1998 <2267661826@qq.com>
2025-01-07 00:15:41 +0800

    gameState,大厅状态（玩家状态）

---
+++ Source/DedicatedServers/Private/Game/DS_GameState.cpp
@@ -0,0 +1,53 @@
+﻿// // Copyright invi1998
+
+
+#include "Game/DS_GameState.h"
+
+#include "Lobby/LobbyState.h"
+#include "Net/UnrealNetwork.h"
+
+ADS_GameState::ADS_GameState()
+{
+	bReplicates = true;
+}
+
+void ADS_GameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ADS_GameState, LobbyState);
+	
+}
+
+void ADS_GameState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		CreateLobbyState();
+		OnLobbyStateInitialized.Broadcast(LobbyState);
+	}
+}
+
+void ADS_GameState::CreateLobbyState()
+{
+	if (UWorld* World = GetWorld(); IsValid(World))
+	{
+		FActorSpawnParameters SpawnInfo;
+		LobbyState = World->SpawnActor<ALobbyState>(
+			ALobbyState::StaticClass(),
+			FVector::ZeroVector,
+			FRotator::ZeroRotator,
+			SpawnInfo);
+		if (IsValid(LobbyState))
+		{
+			LobbyState->SetOwner(this);
+		}
+	}
+}
+
+void ADS_GameState::OnRep_LobbyState()
+{
+	OnLobbyStateInitialized.Broadcast(LobbyState);
+}
---
+++ Source/DedicatedServers/Public/Game/DS_GameState.h
@@ -0,0 +1,40 @@
+﻿// // Copyright invi1998
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "DS_GameState.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnLobbyStateInitialized, ALobbyState*, LobbyState);
+
+class ALobbyState;
+/**
+ * 
+ */
+UCLASS()
+class DEDICATEDSERVERS_API ADS_GameState : public AGameState
+{
+	GENERATED_BODY()
+
+public:
+	ADS_GameState();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(ReplicatedUsing=OnRep_LobbyState)
+	TObjectPtr<ALobbyState> LobbyState;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnLobbyStateInitialized OnLobbyStateInitialized;		// 大厅状态初始化委托
+
+protected:
+	virtual void BeginPlay() override;
+
+	void CreateLobbyState();
+
+private:
+	UFUNCTION()
+	void OnRep_LobbyState();
+	
+};

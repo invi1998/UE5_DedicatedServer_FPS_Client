5038db3895fbe04508912870f7749022fc81f499
invi1998 <2267661826@qq.com>
2025-01-06 23:19:55 +0800

    LobbyState(支持差异化网络复制）

---
+++ Source/DedicatedServers/Private/Lobby/LobbyState.cpp
@@ -0,0 +1,38 @@
+﻿// // Copyright invi1998
+
+
+#include "Lobby/LobbyState.h"
+
+#include "Net/UnrealNetwork.h"
+
+
+// Sets default values
+ALobbyState::ALobbyState()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;		// 开启复制
+	bAlwaysRelevant = true;		// 始终相关
+}
+
+void ALobbyState::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ALobbyState, PlayerInfoArray);		// 复制玩家信息数组
+}
+
+void ALobbyState::AddPlayerInfo(const FLobbyPlayerInfo& PlayerInfo)
+{
+	PlayerInfoArray.AddPlayer(PlayerInfo);
+}
+
+void ALobbyState::RemovePlayerInfo(const FString& PlayerId)
+{
+	PlayerInfoArray.RemovePlayer(PlayerId);
+}
+
+void ALobbyState::OnRep_LobbyPlayerInfoArray()
+{
+}
+
+
---
+++ Source/DedicatedServers/Public/Lobby/LobbyState.h
@@ -0,0 +1,40 @@
+﻿// // Copyright invi1998
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LobbyPlayerInfo.h"
+#include "GameFramework/Info.h"
+#include "LobbyState.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPlayerInfoChanged, const FLobbyPlayerInfo&, PlayerInfo);
+
+UCLASS()
+class DEDICATEDSERVERS_API ALobbyState : public AInfo
+{
+	GENERATED_BODY()
+
+public:
+	ALobbyState();
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnPlayerInfoChanged OnPlayerInfoAdded;	// 玩家添加委托
+
+	UPROPERTY(BlueprintAssignable)
+	FOnPlayerInfoChanged OnPlayerInfoRemoved;	// 玩家移除委托
+
+	void AddPlayerInfo(const FLobbyPlayerInfo& PlayerInfo);
+	void RemovePlayerInfo(const FString& PlayerId);
+	
+protected:
+	UFUNCTION()
+	void OnRep_LobbyPlayerInfoArray();
+
+private:
+	UPROPERTY(Replicated)
+	FLobbyPlayerInfoArray PlayerInfoArray;
+	
+
+};

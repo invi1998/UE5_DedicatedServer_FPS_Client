47d9ff9c344eeffdd0b9c36159955eec930c22e9
invi1998 <2267661826@qq.com>
2025-01-07 00:18:40 +0800

    ULobbyPlayerBox

---
+++ Source/DedicatedServers/Public/UI/Lobby/LobbyPlayerBox.h
@@ -0,0 +1,16 @@
+﻿// // Copyright invi1998
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LobbyPlayerBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEDICATEDSERVERS_API ULobbyPlayerBox : public UUserWidget
+{
+	GENERATED_BODY()
+};

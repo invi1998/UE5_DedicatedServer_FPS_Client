ff4cb53673a6e28f6b22460f690b0db9acd72e1c
invi1998 <2267661826@qq.com>
2025-01-08 17:15:26 +0800

    testUI

---
+++ Source/DedicatedServers/Private/UI/APITest/APITestRecordMatchStatsOverlay.cpp
@@ -0,0 +1,50 @@
+﻿// // Copyright invi1998
+
+
+#include "UI/APITest/APITestRecordMatchStatsOverlay.h"
+
+#include "Components/Button.h"
+#include "HTTP/HTTPRequestTypes.h"
+#include "Player/DSLocalPlayerSubsystem.h"
+#include "UI/GameStats/GameStatsManager.h"
+
+void UAPITestRecordMatchStatsOverlay::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	GameStatsManager = NewObject<UGameStatsManager>(this, GameStatsManagerClass);
+	Button_RecordMatchStats->OnClicked.AddDynamic(this, &UAPITestRecordMatchStatsOverlay::RecordMatchStatsButtonClicked);
+	
+}
+
+void UAPITestRecordMatchStatsOverlay::RecordMatchStatsButtonClicked()
+{
+	UDSLocalPlayerSubsystem* LocalPlayerSubsystem = GetOwningLocalPlayer()->GetSubsystem<UDSLocalPlayerSubsystem>();
+	if (IsValid(LocalPlayerSubsystem))
+	{
+		FDSRecordMatchStatsInput MatchStatsInput;
+		MatchStatsInput.userName = LocalPlayerSubsystem->UserName;
+
+		FDSMatchStats MatchStats;
+
+		MatchStats.defeats = FMath::RandRange(0, 10);
+		MatchStats.hits = FMath::RandRange(0, 10);
+		MatchStats.misses = FMath::RandRange(0, 10);
+		MatchStats.dethroneElims = FMath::RandRange(0, 10);
+		MatchStats.highestStreak = FMath::RandRange(0, 10);
+		MatchStats.matchLosses = FMath::RandRange(0, 10);
+		MatchStats.matchWins = FMath::RandRange(0, 10);
+		MatchStats.revengeElims = FMath::RandRange(0, 10);
+		MatchStats.scoredElims = FMath::RandRange(0, 10);
+		MatchStats.gotFirstBlood = FMath::RandBool();
+		MatchStats.headShotElims = FMath::RandRange(0, 10);
+		MatchStats.showStopperElims = FMath::RandRange(0, 10);
+		
+		MatchStatsInput.matchStats = MatchStats;
+
+		if (IsValid(GameStatsManager))
+		{
+			GameStatsManager->RecordMatchStats(MatchStatsInput);
+		}
+	}
+}
---
+++ Source/DedicatedServers/Public/UI/APITest/APITestRecordMatchStatsOverlay.h
@@ -0,0 +1,36 @@
+﻿// // Copyright invi1998
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "APITestRecordMatchStatsOverlay.generated.h"
+
+class UGameStatsManager;
+class UButton;
+
+/**
+ * 
+ */
+UCLASS()
+class DEDICATEDSERVERS_API UAPITestRecordMatchStatsOverlay : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> Button_RecordMatchStats;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UGameStatsManager> GameStatsManagerClass;
+
+protected:
+	virtual void NativeConstruct() override;
+
+private:
+	UPROPERTY()
+	TObjectPtr<UGameStatsManager> GameStatsManager;
+
+	UFUNCTION()
+	void RecordMatchStatsButtonClicked();
+};
